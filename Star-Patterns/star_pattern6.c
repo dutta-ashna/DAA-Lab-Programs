9a56af58a908910d752575795291f387a96dbf15
Ashna Dutta <Ashna.27808@stu.upes.ac.in>
2026-08-20 20:37:03 +0530

    Added star pattern programs

---
+++ Star-Patterns/star_pattern6.c
@@ -0,0 +1,16 @@
+//Print a pyramid
+#include <stdio.h>
+int main() {
+    int i, j, k, rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for (i=1; i<=rows; i++) {
+        for (k=rows-1; k>=i; k--) {
+            printf(" ");
+        }
+        for (j=1; j<=(2*i-1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
